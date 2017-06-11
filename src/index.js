fed7c4fae2215b7cdb42c1a7d2b41d1cb6db5489
Tuan Duong <tuanduong15@gmail.com>
2017-06-11 00:54:49 -0400

    Create index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import { render } from 'react-dom'; // lets us render into our html
 import App from 'components/app';
 
 render(<App />, document.getElementById('app'));
+
+//2 days since your last commit! 
